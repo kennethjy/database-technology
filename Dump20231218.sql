2bba52f3d1f72875509ebcdf1ca65482abd702c5
kennethjy <114073455+kennethjy@users.noreply.github.com>
2023-12-18 23:50:07 +0700

    Add files via upload

---
+++ Dump20231218.sql
@@ -0,0 +1,194 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: databasesproject
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `delivery`
+--
+
+DROP TABLE IF EXISTS `delivery`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `delivery` (
+  `trackingID` int DEFAULT NULL,
+  `transactionID` int NOT NULL,
+  `deliveryMethod` varchar(45) DEFAULT NULL,
+  `Provider` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`transactionID`),
+  CONSTRAINT `deliveryTransactionID` FOREIGN KEY (`transactionID`) REFERENCES `transaction` (`transactionID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `delivery`
+--
+
+LOCK TABLES `delivery` WRITE;
+/*!40000 ALTER TABLE `delivery` DISABLE KEYS */;
+INSERT INTO `delivery` VALUES (101001,20231118,'land','JNE'),(101002,20231129,'sea','permata ship');
+/*!40000 ALTER TABLE `delivery` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `duties`
+--
+
+DROP TABLE IF EXISTS `duties`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `duties` (
+  `roleID` int NOT NULL,
+  `Description` varchar(45) DEFAULT NULL,
+  `Pay` int DEFAULT NULL,
+  PRIMARY KEY (`roleID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `duties`
+--
+
+LOCK TABLES `duties` WRITE;
+/*!40000 ALTER TABLE `duties` DISABLE KEYS */;
+INSERT INTO `duties` VALUES (1,'land',7000000),(2,'sea',8000000),(3,'air',9000000);
+/*!40000 ALTER TABLE `duties` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `EmployeeID` int NOT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `Email` varchar(45) DEFAULT NULL,
+  `RoleID` int DEFAULT NULL,
+  PRIMARY KEY (`EmployeeID`),
+  KEY `EmployeeRoleID_idx` (`RoleID`),
+  CONSTRAINT `EmployeeRoleID` FOREIGN KEY (`RoleID`) REFERENCES `duties` (`roleID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (2023001,'kennan','kennan@gmail.com',1),(2023002,'kenneth','kenneth@gmail.com',2),(2023003,'nick','nick@gmail.com',3),(2023004,'vania','vania@gmail.com',3),(2023005,'jess','jess@mail.com',2);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `itemstock`
+--
+
+DROP TABLE IF EXISTS `itemstock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `itemstock` (
+  `itemID` int NOT NULL,
+  `itemName` varchar(45) DEFAULT NULL,
+  `stockAmount` int DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  PRIMARY KEY (`itemID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `itemstock`
+--
+
+LOCK TABLES `itemstock` WRITE;
+/*!40000 ALTER TABLE `itemstock` DISABLE KEYS */;
+INSERT INTO `itemstock` VALUES (1,'apple',115,15000),(2,'orange',120,18000),(3,'banana',300,23000),(4,'grape',150,28000),(5,'avocado',250,40000);
+/*!40000 ALTER TABLE `itemstock` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sales`
+--
+
+DROP TABLE IF EXISTS `sales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sales` (
+  `saleID` int NOT NULL,
+  `amount` int DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `transactionID` int DEFAULT NULL,
+  `itemID` int DEFAULT NULL,
+  PRIMARY KEY (`saleID`),
+  KEY `itemID_idx` (`itemID`),
+  KEY `transactionID_idx` (`transactionID`),
+  CONSTRAINT `salesItemID` FOREIGN KEY (`itemID`) REFERENCES `itemstock` (`itemID`),
+  CONSTRAINT `salesTransactionID` FOREIGN KEY (`transactionID`) REFERENCES `transaction` (`transactionID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sales`
+--
+
+LOCK TABLES `sales` WRITE;
+/*!40000 ALTER TABLE `sales` DISABLE KEYS */;
+INSERT INTO `sales` VALUES (990001,40,17000,20231118,1),(990002,40,20000,20231118,2),(990003,150,25000,20231118,3),(990004,40,30000,20231118,4),(990005,40,42000,20231118,5),(990006,50,10000,20231129,1),(990007,60,20000,20231129,4),(990008,40,15000,20231129,2),(990009,15,10000,20231130,1),(990010,20,12000,20231130,2);
+/*!40000 ALTER TABLE `sales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction` (
+  `transactionID` int NOT NULL,
+  `CompanyName` varchar(45) DEFAULT NULL,
+  `Delivery` datetime NOT NULL,
+  `Arrival` datetime DEFAULT NULL,
+  `isExport` tinyint NOT NULL,
+  `EmployeeID` int DEFAULT NULL,
+  PRIMARY KEY (`transactionID`),
+  KEY `TransactionEmployeeID_idx` (`EmployeeID`),
+  CONSTRAINT `TransactionEmployeeID` FOREIGN KEY (`EmployeeID`) REFERENCES `employee` (`EmployeeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+INSERT INTO `transaction` VALUES (20231118,'ovals','2023-11-13 14:30:12','2023-11-17 20:12:33',1,2023001),(20231129,'adyas','2023-11-20 13:30:21','2023-11-28 15:16:16',0,2023005),(20231130,'hilkias','2023-12-18 19:41:55','2023-12-25 19:41:55',0,2023001);
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-18 23:48:56
