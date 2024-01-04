e038408fde6318cc1b59bdb1e53c3066e9a920bf
kennethjy <114073455+kennethjy@users.noreply.github.com>
2024-01-04 23:19:33 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import datetime
+import os
 
 
 connect = mysql.connector.connect(
@@ -9,17 +10,36 @@ connect = mysql.connector.connect(
     database='databasesproject'
 )
 
+
+def deleteSales(transactionID, itemID, connect):
+    db = connect.cursor()
+    db.execute("SELECT sales.SaleID, sales.amount, transaction.isExport FROM sales "
+               "JOIN transaction ON transaction.TransactionID = sales.TransactionID "
+               "WHERE sales.TransactionID = %s AND sales.ItemID = %s",
+               (transactionID, itemID))
+    entry = db.fetchone()
+    salesID, amount, isExport = entry[0], entry[1], entry[2]
+    db.execute("DELETE FROM sales WHERE SaleID = %s", (salesID,))
+    db.execute("SELECT stockAmount FROM itemstock WHERE itemID = %s", (itemID,))
+    stockamount = db.fetchone()[0]
+    db.execute("UPDATE itemstock SET stockAmount = %s WHERE itemID = %s",
+               ((stockamount + amount) if isExport else (stockamount - amount), itemID))
+    connect.commit()
+
+
 def getRoleID(role, connect):
     db = connect.cursor()
     db.execute(fr"SELECT RoleID FROM duties WHERE Description = %s", (role,))
     ID = db.fetchone()[0]
     return ID
 
+
 def getName(employeeID, connect):
     db = connect.cursor()
     db.execute(fr"SELECT name FROM employee WHERE EmployeeID = %s", (employeeID,))
     ID = db.fetchone()[0]
     return ID
+
 
 def login(name, connect):
     db = connect.cursor()
@@ -32,6 +52,8 @@ def changeRole(name, roleID, connect):
     db = connect.cursor()
     db.execute("UPDATE employee SET RoleID = %s WHERE name = %s", (roleID, name))
     connect.commit()
+    db.execute("SELECT Description FROM duties WHERE RoleID = %s ", (roleID,))
+    print(f"Role changed to {db.fetchone()[0]}")
 
 
 def getItem(itemName, connect):
@@ -75,7 +97,7 @@ def newSales(transaction, itemID, amount, price, connect):
 
 def getBill(transID, connect):
     db = connect.cursor()
-    db.execute("SELECT itemstock.itemName, sales.amount, sales.price FROM sales "
+    db.execute("SELECT itemstock.itemName, sales.amount, itemstock.price FROM sales "
                "JOIN itemstock ON sales.itemID = itemstock.itemID "
                "WHERE sales.transactionID = %s;", (transID,))
     result = db.fetchall()
@@ -91,14 +113,40 @@ def processBill(transID, connect):
     db.execute("SELECT isExport FROM transaction WHERE transactionID = %s", (transID,))
     isExport = db.fetchone()[0]
     while True:
+        getItems(connect)
         itemID = getItem(input(f"Enter item name to {'import' if not isExport else 'export'}: "), connect)
         amt = int(input(f"Enter amount of items to {'import' if not isExport else 'export'}: "))
-        price = int(input(f"Enter price to {'import' if not isExport else 'export'} at: "))
+        if isExport:
+            db.execute("SELECT stockAmount FROM itemStock WHERE itemID = %s", (itemID,))
+            amountInStock = db.fetchone()[0]
+            if amountInStock < amt:
+                print("Out of stock")
+                continue
+        db.execute("SELECT price FROM itemstock "
+               "WHERE itemID = %s;", (itemID,))
+        price = db.fetchone()[0]
         newSales(transID, itemID, amt, price, connect)
         if input("Close bill? [Y/N]: ") == 'Y':
             getBill(transID, connect)
+            while True:
+                if input("Delete any items? [Y/N]: ") == 'Y':
+                    deleteSales(transID, getItem(input("Enter item name to delete: "), connect), connect)
+                    if input("Add any items? [Y/N]: ") == 'Y':
+                        processBill(transID, connect)
+                    else:
+                        break
+                else:
+                    break
+                break
             break
 
+def getItems(connect):
+    db = connect.cursor()
+    db.execute("SELECT itemName, stockAmount, price FROM itemStock")
+    items = db.fetchall()
+    print(f'Name\tPrice\tAmount')
+    for i in items:
+        print(f'{i[0]}\t{i[2]}\t\t{i[1]}')
 
 employee = login(input("Enter your name: "), connect)
 
@@ -110,6 +158,8 @@ select = input("Enter selection: ")
 if select == '2':
     isExport = True if input("Import or Export? [I/E]:") == 'E' else False
     company = input(f"Enter company name to {'import from'if not isExport else 'export to'}: ")
+    print('\n\n')
+
     processBill(newTransaction(isExport, company, employee, connect), connect)
 
 else:
